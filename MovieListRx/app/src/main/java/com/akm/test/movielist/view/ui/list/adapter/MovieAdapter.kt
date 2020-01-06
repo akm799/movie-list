aea63e8199e661056f2a05df5a4632378c8a4e87
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2020-01-06 21:26:20 +0000

    Aligned the adapters in both implementations.

--- MovieListRx/app/src/main/java/com/akm/test/movielist/view/ui/list/adapter/MovieAdapter.kt
+++ MovieListRx/app/src/main/java/com/akm/test/movielist/view/ui/list/adapter/MovieAdapter.kt
@@ -2,38 +2,33 @@ package com.akm.test.movielist.view.ui.list.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.akm.test.movielist.R
 import com.akm.test.movielist.view.processor.MovieProcessor
+import com.akm.test.movielist.view.ui.list.adapter.helper.LastItemAdapter
 
 /**
  * This adapter contains no separate data holding element or business logic. Both are functions of
  * the processor.
  */
-class MovieAdapter(private val movieProcessor: MovieProcessor) : RecyclerView.Adapter<MovieViewHolder>() {
-    private companion object {
-        const val ELEMENT_TYPE = 0
-        const val LAST_ELEMENT_TYPE = 1
+class MovieAdapter(private val movieProcessor: MovieProcessor) : LastItemAdapter<MovieViewHolder>() {
+
+    override fun onCreateItemViewHolder(parent: ViewGroup): MovieViewHolder {
+        return buildMovieViewHolder(parent, R.layout.item_movie)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val viewResId = if (viewType == LAST_ELEMENT_TYPE) R.layout.item_movie_last else R.layout.item_movie
+    override fun onCreateLastItemViewHolder(parent: ViewGroup): MovieViewHolder {
+        return buildMovieViewHolder(parent, R.layout.item_movie_last)
+    }
+
+    private fun buildMovieViewHolder(parent: ViewGroup, viewResId: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(viewResId, parent, false)
 
         return MovieViewHolder(view, movieProcessor)
     }
 
-    override fun getItemCount(): Int = movieProcessor.movies.size + 1
+    override fun getNonLastItemCount(): Int = movieProcessor.movies.size
 
-    override fun getItemViewType(position: Int): Int {
-        return if (isLastElement(position)) LAST_ELEMENT_TYPE else ELEMENT_TYPE
+    override fun onBindItemViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bindMovie(movieProcessor.movies[position], position)
     }
-
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        if (isLastElement(position).not()) {
-            holder.bindMovie(movieProcessor.movies[position], position)
-        }
-    }
-
-    private fun isLastElement(position: Int): Boolean = (position == itemCount - 1)
 }
